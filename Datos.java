3b7d4690207ad7964fae7a92f4d4526348abb55c
jonathan4747 <harold307lat@gmail.com>
2022-02-14 16:04:04 -0500

    mi primer codigo

---
+++ Datos.java
@@ -0,0 +1,10 @@
+public class Datos {
+    public static void main (String args[]){
+        int edad = 100;
+        String ciudad = "Burbank, CA";
+        String nombre = "Coding Dojo";
+        System.out.println("Mi nombre es " + nombre);
+        System.out.println("Tengo "+edad+" de edad" );
+        System.out.println("Mi ciudad natal es "+ ciudad);
+    }
+}
